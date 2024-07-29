8f886708e35bff0863127db405d4967dfa5dec5a
master lakshya <lakshyasharma11042006@gmail.com>
2024-07-29 12:08:57 +0530

    commited

---
+++ variables.js
@@ -0,0 +1,29 @@
+// a=5;
+//  function variable(){
+     
+// {
+//     let a = 15;
+//          let b = 5;
+//         console.log(a);
+//         console.log(a);
+// }
+// }
+         
+// variable();
+// console.log(a);
+
+// // a =5;
+
+// // a =10;
+// // console.log(a);
+
+// // let a =10;
+// // var b= 10;
+
+function sum(a,b){
+    let s = a+b;
+    console.log(`the sum of ${a} and ${b} is ${s}`);
+
+  
+} 
+ sum(3,5);
